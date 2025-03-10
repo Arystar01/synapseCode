423d7706a632d94a3c30084ab7f63a38e9effbab
29deepanshutyagi <29deepanshutyagi@gmail.com>
2025-03-10 20:52:45 +0530

    first commit

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/Haxplore-main', // Replace with your repository name
+}
+
+module.exports = nextConfig 
